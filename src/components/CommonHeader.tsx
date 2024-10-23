43b1cba0884dbb6b47544137ceb8d562140c37bb
Faysel Abdella <fayselcode@gmail.com>
2024-10-23 11:34:10 +0300

    update title

--- src/components/CommonHeader.tsx
+++ src/components/CommonHeader.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
 interface CommonHeaderProps {
-  title: string; // Define the title prop type as a string
+  title?: string; // Define the title prop type as a string
 }
 
 const CommonHeader: React.FC<CommonHeaderProps> = ({ title }) => {
   return (
     <>
-      <div className='flex justify-end'>
-        <div className='flex gap-2 items-center mb-4'>
-          <div className='text-mainGray'>관리자 님</div>
-          <button className='bg-[#A2ABAF] px-3 py-1 rounded-sm font-normal text-base text-right text-white'>
+      <div className="flex justify-end">
+        <div className="mb-4 flex items-center gap-2">
+          <div className="text-mainGray">관리자 님</div>
+          <button className="rounded-sm bg-[#A2ABAF] px-3 py-1 text-right text-base font-normal text-white">
             관리자 님
           </button>
         </div>
       </div>
-      <h2 className='mt-4 font-bold text-[30px] leading-[42px] text-center text-mainBlack'>
+      <h2 className="mt-4 text-center text-[30px] font-bold leading-[42px] text-mainBlack">
         {title} {/* Render the title passed as a prop */}
       </h2>
     </>
